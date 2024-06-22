a8aac40d885fa82ec5f20b7f348a5dc594bc8c9f
Davide Mucci <davidemucci0804@gmail.com>
2024-06-22 10:25:14 +0200

    add-task-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 // CORE
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DUMMY_USERS } from '../assets/users/dummy-users';
 // COMPONENTS
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from "./components/tasks/tasks.component";
 import { UserComponent } from "./components/user/user.component";
+// MODELS
+import { DUMMY_USERS } from '../assets/users/dummy-users';
 import { User } from './models/User';
 
 @Component({
@@ -12,14 +14,16 @@ import { User } from './models/User';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [HeaderComponent, RouterOutlet, UserComponent]
+    imports: [HeaderComponent, RouterOutlet, TasksComponent, UserComponent]
 })
 export class AppComponent {
   title = 'first-angular-app';
   users: User[] = DUMMY_USERS;
+  userName: string = 'Please select a user to view his tasks';
 
   handleSelect(id: string){
-    console.log(id);
-    
+    let name = DUMMY_USERS.find(u => u.id === id)?.name;
+    this.userName = name ?? this.userName;
   }
 }
+ 
